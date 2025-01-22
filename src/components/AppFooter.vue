22357721aa938ef75c88a589224168d77ec0bdca
Jose6348 <jfalasco88@gmail.com>
2025-01-21 23:11:18 -0300

    style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer>
-    <p>© 2023 Gerenciador de Finanças Pessoais</p>
+  <footer class="app-footer">
+    <p class="footer-text">© 2025 Gerenciador de Finanças Pessoais</p>
   </footer>
 </template>
 
@@ -9,3 +9,21 @@ export default {
   name: 'AppFooter'
 }
 </script>
+
+<style scoped>
+.app-footer {
+  background-color: #34495e;
+  color: white;
+  padding: 15px 0;
+  text-align: center;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+}
+
+.footer-text {
+  margin: 0;
+  font-size: 14px;
+}
+</style>
