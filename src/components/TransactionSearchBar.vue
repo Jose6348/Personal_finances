311b3953557c5bfbf3be34d0cd183f304c8f12de
Jose6348 <jfalasco88@gmail.com>
2025-01-21 22:45:46 -0300

    Search

---
+++ src/components/TransactionSearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <input type="text" v-model="searchTerm" placeholder="Buscar transação..." @input="searchTransactions">
+  </template>
+  
+  <script>
+  export default {
+    name: 'TransactionSearchBar',
+    data() {
+      return {
+        searchTerm: ''
+      };
+    },
+    methods: {
+      searchTransactions() {
+        this.$emit('search', this.searchTerm);
+      }
+    }
+  };
+  </script>
