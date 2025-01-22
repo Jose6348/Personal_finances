a6a003b93ca2f35164789a56993d2606fda6513d
Jose6348 <jfalasco88@gmail.com>
2025-01-21 22:45:23 -0300

    Sumario

---
+++ src/components/FinancialSummary.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h2>Resumo Financeiro</h2>
+      <div>
+        <p>Saldo Total: {{ formatCurrency(totalBalance) }}</p>
+        <p>Total de Receitas: {{ formatCurrency(totalIncome) }}</p>
+        <p>Total de Despesas: {{ formatCurrency(totalExpenses) }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FinancialSummary',
+    computed: {
+      totalBalance() {
+        return this.$store.getters.totalBalance;
+      },
+      totalIncome() {
+        return this.$store.getters.totalIncome;
+      },
+      totalExpenses() {
+        return this.$store.getters.totalExpenses;
+      }
+    },
+    methods: {
+      formatCurrency(value) {
+        return `R$ ${value.toFixed(2)}`;
+      }
+    }
+  };
+  </script>
