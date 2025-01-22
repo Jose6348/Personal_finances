8d8a3618059bcb2d97ceaded17136843f6d13607
Jose6348 <jfalasco88@gmail.com>
2025-01-21 22:45:40 -0300

    Notificacao

---
+++ src/components/TransactionNotification.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="notification" :class="notificationType === 'success' ? 'success' : 'error'">
+      {{ message }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TransactionNotification',
+    props: {
+      notificationType: {
+        type: String,
+        default: 'success'
+      },
+      message: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .notification {
+    padding: 10px;
+    margin: 10px 0;
+    border-radius: 4px;
+    color: white;
+  }
+  
+  .success {
+    background-color: #4CAF50;
+  }
+  
+  .error {
+    background-color: #F44336;
+  }
+  </style>
