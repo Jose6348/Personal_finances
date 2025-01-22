1a305f2b0f5758b66900708ed635b63efaddd35e
Jose6348 <jfalasco88@gmail.com>
2025-01-21 22:45:28 -0300

    Itens

---
+++ src/components/TransactionItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li>
+      {{ transaction.description }} - {{ transaction.type }}: {{ transaction.amount | currency }}
+      <button @click="editTransaction">Editar</button>
+      <button @click="deleteTransaction">Excluir</button>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props: ['transaction'],
+    methods: {
+      editTransaction() {
+        // Lógica para editar a transação
+      },
+      deleteTransaction() {
+        this.$store.commit('deleteTransaction', this.transaction.id);
+      }
+    },
+    filters: {
+      currency(value) {
+        return `R$ ${value.toFixed(2)}`;
+      }
+    }
+  };
+  </script>
