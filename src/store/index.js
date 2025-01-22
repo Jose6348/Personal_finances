a529bc0de7f900d95c449d0a22253814bce868bc
Jose6348 <jfalasco88@gmail.com>
2025-01-21 22:45:59 -0300

    Actions

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    transactions: [],
+    categories: ['Alimentação', 'Transporte', 'Saúde', 'Lazer'],
+    goals: []
+  },
+  mutations: {
+    // Suas mutações existentes aqui
+  },
+  actions: {
+    // Suas ações aqui
+  },
+  getters: {
+    totalBalance: state => {
+      return state.transactions.reduce((acc, transaction) => {
+        return acc + (transaction.type === 'income' ? transaction.amount : -transaction.amount);
+      }, 0);
+    },
+    totalIncome: state => {
+      return state.transactions.reduce((acc, transaction) => {
+        return transaction.type === 'income' ? acc + transaction.amount : acc;
+      }, 0);
+    },
+    totalExpenses: state => {
+      return state.transactions.reduce((acc, transaction) => {
+        return transaction.type === 'expense' ? acc + transaction.amount : acc;
+      }, 0);
+    }
+  }
+})
