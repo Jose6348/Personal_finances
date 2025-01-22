162973d472a8d2967e41e6cde28e6ca4b0a565a2
Jose6348 <jfalasco88@gmail.com>
2025-01-21 22:45:01 -0300

    Cabeçalho

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>Gerenciador de Finanças Pessoais</h1>
+    <nav>
+      <router-link to="/">Início</router-link> | 
+      <router-link to="/transactions">Transações</router-link> | 
+      <router-link to="/goals">Metas</router-link> | 
+      <router-link to="/summary">Resumo</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
